f48b95f891a1ea3822706c6dc7db40a6dd161b61
Jonas Van Mullem <jonasvanmullem@hotmail.com>
2020-08-10 13:34:34 +0200

    oef03 plus bonussen afgewerkt

---
+++ Oef03/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Oef03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxStringList = new System.Windows.Forms.ListBox();
+            this.txbFilter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbxStringList
+            // 
+            this.lbxStringList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxStringList.FormattingEnabled = true;
+            this.lbxStringList.Location = new System.Drawing.Point(12, 12);
+            this.lbxStringList.Name = "lbxStringList";
+            this.lbxStringList.Size = new System.Drawing.Size(120, 95);
+            this.lbxStringList.TabIndex = 0;
+            // 
+            // txbFilter
+            // 
+            this.txbFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbFilter.Location = new System.Drawing.Point(13, 114);
+            this.txbFilter.Name = "txbFilter";
+            this.txbFilter.Size = new System.Drawing.Size(119, 20);
+            this.txbFilter.TabIndex = 1;
+            this.txbFilter.TextChanged += new System.EventHandler(this.txbFilter_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(167, 157);
+            this.Controls.Add(this.txbFilter);
+            this.Controls.Add(this.lbxStringList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxStringList;
+        private System.Windows.Forms.TextBox txbFilter;
+    }
+}
+
---
+++ Oef03/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oef03
+{
+    public partial class Form1 : Form
+    {
+        List<string> lijst = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lijst.Add("An");
+            lijst.Add("Bart");
+            lijst.Add("Cedric");
+            lijst.Add("Dieter");
+            lijst.Add("Evert");
+            lijst.Add("Filip");
+            lijst.Add("Gert");
+            lijst.Add("Henk");
+            foreach (var item in lijst)
+            {
+                lbxStringList.Items.Add(item);
+            }
+        }
+
+
+        private void txbFilter_TextChanged(object sender, EventArgs e)
+        {
+            lbxStringList.Items.Clear();
+            foreach (var item in lijst)
+            {
+                if (item.Contains(txbFilter.Text))
+                {
+                    lbxStringList.Items.Add(item);
+                }
+            }
+        }
+    }
+}
