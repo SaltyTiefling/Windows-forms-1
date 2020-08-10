acda647609ba29be1a2425a45cd31d360eaab34f
Jonas Van Mullem <jonasvanmullem@hotmail.com>
2020-08-10 14:33:46 +0200

    oef04 afgewerkt

---
+++ Oef04/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oef04
+{
+    class User
+    {
+        public string voornaam { get; set; }
+        public string achternaam { get; set; }
+
+        public User(string voornaam, string achternaam)
+        {
+            this.voornaam = voornaam;
+            this.achternaam = achternaam;
+        }
+
+
+
+    }
+}
