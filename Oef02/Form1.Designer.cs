17a6a19c1a3e4b074b86cf04bd9fbe7f3a0d0195
Jonas Van Mullem <jonasvanmullem@hotmail.com>
2020-08-10 13:20:08 +0200

    heb oef 2 afgewerkt samen met de bonussen opdrachten

---
+++ Oef02/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Oef02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxItems = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxItems
+            // 
+            this.lbxItems.FormattingEnabled = true;
+            this.lbxItems.Location = new System.Drawing.Point(12, 12);
+            this.lbxItems.Name = "lbxItems";
+            this.lbxItems.Size = new System.Drawing.Size(237, 251);
+            this.lbxItems.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 269);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(237, 30);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Verwijder item";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 312);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lbxItems);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxItems;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Oef02/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oef02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lbxItems.Items.Count <= 0)
+            {
+                MessageBox.Show("er zijn geen items meer");
+            }
+            else if (lbxItems.SelectedIndex < 0)
+            {
+                MessageBox.Show("selteer een item A.U.B.");
+            }
+            else
+            {
+                MessageBox.Show(lbxItems.SelectedItem.ToString());
+                lbxItems.Items.Remove(lbxItems.SelectedItem);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbxItems.Items.Add("jonas");
+            lbxItems.Items.Add("nog eens jonas");
+            lbxItems.Items.Add("jonas again");
+            lbxItems.Items.Add("jonas wants your lovin\'");
+            lbxItems.Items.Add("jonas is bored but in a list woohoo");
+        }
+    }
+}
